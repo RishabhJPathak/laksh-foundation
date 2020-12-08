3bc0e59d217923807cd4c7a6df730278341cef93
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-08 22:36:47 +0530

    added routes

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,23 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.render("index", { title: "Laksh Foundation - Home" });
 });
+router.get("/about", (req, res) => {
+  res.render("about", { title: "Laksh Foundation - About", page: "about" });
+});
+router.get("/events", (req, res) => {
+  res.render("events", { title: "Laksh Foundation - Events", page: "events" });
+});
+router.get("/gallery", (req, res) => {
+  res.render("gallery", {
+    title: "Laksh Foundation - Gallery",
+    page: "gallery",
+  });
+});
+router.get("/contact", (req, res) => {
+  res.render("contact", {
+    title: "Laksh Foundation - Contact",
+    page: "contact",
+  });
+});
 
 module.exports = router;
