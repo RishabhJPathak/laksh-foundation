0e84912de9d54c5f4ccd1eb715520f90fd14ebcf
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-09 16:48:09 +0530

    added pages object in home render, to check active class

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "Laksh Foundation - Home" });
+  res.render("index", { title: "Laksh Foundation - Home", page: "home" });
 });
 router.get("/about", (req, res) => {
   res.render("about", { title: "Laksh Foundation - About", page: "about" });
