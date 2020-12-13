994637e1ea98438cd1c0eaf1be9601b4c66d4316
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-13 19:52:16 +0530

    removed pug and page renders

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const path = require("path");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const indexRouter = require("./routes");
 
 app.use(express.static(path.join(__dirname, "public")));
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+require("./init/nodemailer");
 
 app.use("/", indexRouter);
 
